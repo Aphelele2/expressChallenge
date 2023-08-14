9f93e8cd503167e9ca5a7a1dff755d80f55222a8
Aphelele2 <aphelelejoyi@gmail.com>
2023-08-14 14:39:48 +0200

    controller

--- API/controller/index.js
+++ API/controller/index.js
@@ -1,1 +1,34 @@
-// conroller
+// controller
+const express = require('express')
+const bodyParser = require('body-parser')
+const routes = express.Router()
+// import all model's objects
+const {users} = require('../model')
+
+// ================  USERS ROUTER  ====================
+
+routes.get('/users', (req, res)=>{
+    users.fetchUsers(req, res)
+})
+routes.get('/user/:id', bodyParser.json(),(req, res)=>{
+    users.updateUser(req, res)
+})
+routes.post('/register', bodyParser.json(),
+(req,res)=>{
+    users.register(req,res)
+})
+routes.put('/user/:id',bodyParser.json(),
+(req,res)=>{
+    users.updateUser(req,res)
+})
+routes.patch('/user/:id',bodyParser.json(),
+(req,res)=>{
+    users.updateUser(req,res)
+})
+routes.delete('/user/:id',(req,res)=>{
+    users.deleteUser(req,res)
+})
+module.exports = {
+    express, 
+    routes
+}
